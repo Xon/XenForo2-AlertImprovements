0321d4c9f28dc6c6953a0149d9f73688ea180e14
Xon <tazipper1@hotmail.com>
2017-09-12 06:45:13 +0800

    Store _output not _data

---
+++ upload/src/addons/SV/AlertImprovements/_output/extension_hint.php
@@ -0,0 +1,18 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+namespace SV\AlertImprovements\XF\Pub\Controller
+{
+	class XFCP_Account extends \XF\Pub\Controller\Account {}
+	class XFCP_Conversation extends \XF\Pub\Controller\Conversation {}
+	class XFCP_Member extends \XF\Pub\Controller\Member {}
+	class XFCP_Report extends \XF\Pub\Controller\Report {}
+	class XFCP_Thread extends \XF\Pub\Controller\Thread {}
+}
+
+namespace SV\AlertImprovements\XF\Repository
+{
+	class XFCP_UserAlert extends \XF\Repository\UserAlert {}
+}
