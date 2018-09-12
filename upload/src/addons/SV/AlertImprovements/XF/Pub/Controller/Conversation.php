9b450b944c7cd7c6e48522c452dbea5b41eb3a39
Xon <tazipper1@hotmail.com>
2018-09-12 14:16:04 +0800

    Support Live Conten (Thread/Conversations)

--- upload/src/addons/SV/AlertImprovements/XF/Pub/Controller/Conversation.php
+++ upload/src/addons/SV/AlertImprovements/XF/Pub/Controller/Conversation.php
@@ -3,6 +3,8 @@
 namespace SV\AlertImprovements\XF\Pub\Controller;
 
 use SV\AlertImprovements\XF\Repository\UserAlert;
+use XF\Entity\ConversationUser;
+use XF\Mvc\Entity\AbstractCollection;
 use XF\Mvc\ParameterBag;
 use XF\Mvc\Reply\View;
 
@@ -28,5 +30,30 @@ class Conversation extends XFCP_Conversation
         }
 
         return $reply;
+    }
+
+    /**
+     * @param ConversationUser $convUser
+     * @param int          $lastDate
+     * @param int          $limit
+     * @return array
+     */
+    protected function _getNextLivePosts(ConversationUser $convUser, $lastDate, $limit = 3)
+    {
+        /** @noinspection PhpUndefinedMethodInspection */
+        /**
+         * @var AbstractCollection $contents
+         * @var int $lastDate
+         */
+        list ($contents, $lastDate) = parent::_getNextLivePosts($convUser, $lastDate, $limit);
+
+        $contentIds  = $contents->keys();
+        $contentType = 'conversation_message';
+
+        /** @var UserAlert $alertRepo */
+        $alertRepo = $this->repository('XF:UserAlert');
+        $alertRepo->markAlertsReadForContentIds($contentType, $contentIds);
+
+        return [$contents, $lastDate];
     }
 }
--- upload/src/addons/SV/AlertImprovements/XF/Pub/Controller/Thread.php
+++ upload/src/addons/SV/AlertImprovements/XF/Pub/Controller/Thread.php
@@ -3,8 +3,10 @@
 namespace SV\AlertImprovements\XF\Pub\Controller;
 
 use SV\AlertImprovements\XF\Repository\UserAlert;
+use XF\Mvc\Entity\AbstractCollection;
 use XF\Mvc\ParameterBag;
 use XF\Mvc\Reply\View;
+use \XF\Entity\Thread as ThreadEntity;
 
 class Thread extends XFCP_Thread
 {
@@ -50,5 +52,30 @@ class Thread extends XFCP_Thread
         }
 
         return $reply;
+    }
+
+    /**
+     * @param ThreadEntity $thread
+     * @param int          $lastDate
+     * @param int          $limit
+     * @return array
+     */
+    protected function _getNextLivePosts(ThreadEntity $thread, $lastDate, $limit = 3)
+    {
+        /** @noinspection PhpUndefinedMethodInspection */
+        /**
+         * @var AbstractCollection $contents
+         * @var int $lastDate
+         */
+        list ($contents, $lastDate) = parent::_getNextLivePosts($thread, $lastDate, $limit);
+
+        $contentIds  = $contents->keys();
+        $contentType = 'post';
+
+        /** @var UserAlert $alertRepo */
+        $alertRepo = $this->repository('XF:UserAlert');
+        $alertRepo->markAlertsReadForContentIds($contentType, $contentIds);
+
+        return [$contents, $lastDate];
     }
 }
