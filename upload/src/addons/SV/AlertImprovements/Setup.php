6e59aefe7650888a1229412eb00373ae92828a26
Xon <tazipper1@hotmail.com>
2020-07-25 05:02:44 +0800

    version dependency bump and depend on new standard lib add-on

--- upload/src/addons/SV/AlertImprovements/Setup.php
+++ upload/src/addons/SV/AlertImprovements/Setup.php
@@ -2,7 +2,7 @@
 
 namespace SV\AlertImprovements;
 
-use SV\Utils\InstallerHelper;
+use SV\StandardLib\InstallerHelper;
 use XF\AddOn\AbstractSetup;
 use XF\AddOn\StepRunnerInstallTrait;
 use XF\AddOn\StepRunnerUninstallTrait;
@@ -16,7 +16,6 @@ use XF\Db\Schema\Alter;
  */
 class Setup extends AbstractSetup
 {
-    // from https://github.com/Xon/XenForo2-Utils cloned to src/addons/SV/Utils
     use InstallerHelper;
     use StepRunnerInstallTrait;
     use StepRunnerUpgradeTrait;
