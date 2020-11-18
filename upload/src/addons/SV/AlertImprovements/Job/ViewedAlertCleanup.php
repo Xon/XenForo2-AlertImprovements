f269ed3fbfb25f41222ca40421aecadeb42a8cd2
Xon <tazipper1@hotmail.com>
2020-11-18 12:40:28 +0800

    Rebuild alert pruning to recompute alert totals, and make XF2.2 compatible

---
+++ upload/src/addons/SV/AlertImprovements/Job/ViewedAlertCleanup.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace SV\AlertImprovements\Job;
+
+use XF\Job\AbstractJob;
+
+class ViewedAlertCleanup extends AbstractJob
+{
+    protected $defaultData = [
+        'cutOff' => 0,
+        'userIds' => [],
+        'pruned' => false,
+    ];
+
+    /**
+     * @inheritDoc
+     */
+    public function run($maxRunTime)
+    {
+        $cutOff = $this->data['cutOff'] ?? 0;
+        $cutOff = (int)$cutOff;
+        if (!$cutOff)
+        {
+            return $this->complete();
+        }
+
+        $startTime = \microtime(true);
+        $db = \XF::db();
+
+        if (empty($this->data['userIds']))
+        {
+            $userIds = $db->fetchAllColumn('
+                SELECT DISTINCT alerted_user_id 
+                FROM xf_user_alert 
+                WHERE view_date > 0 AND view_date < ? AND alerted_user_id <> 0
+            ', $cutOff);
+
+            if (empty($userIds))
+            {
+                return $this->complete();
+            }
+
+            $this->data['userIds'] = $userIds;
+            $this->saveIncrementalData();
+        }
+
+        if (microtime(true) - $startTime >= $maxRunTime)
+        {
+            return $this->resume();
+        }
+
+        if (empty($this->data['pruned']))
+        {
+            /** @var \SV\AlertImprovements\XF\Repository\UserAlertPatch $alertRepo */
+            $alertRepo = \XF::app()->repository('XF:UserAlert');
+            $continue = $alertRepo->pruneViewedAlertsBatch($cutOff, $startTime, $maxRunTime);
+            if ($continue)
+            {
+                $resume = $this->resume();
+                $resume->continueDate = \XF::$time = 1;
+
+                return $resume;
+            }
+
+            $this->data['pruned'] = true;
+            $this->saveIncrementalData();
+        }
+
+        if ($this->data['userIds'])
+        {
+            foreach($this->data['userIds'] as $userId)
+            {
+                $db->beginTransaction();
+
+                /** @var \XF\Entity\User $user */
+                $user = \XF::app()->find('XF:User', $userId);
+                if ($user)
+                {
+                    $alertRepo->updateUnreadCountForUser($user);
+                    $alertRepo->updateUnviewedCountForUser($user);
+                }
+                unset($this->data['userIds'][$userId]);
+                $this->saveIncrementalData();
+
+                $db->commit();
+
+                if (microtime(true) - $startTime >= $maxRunTime)
+                {
+                    return $this->resume();
+                }
+            }
+        }
+
+        return $this->complete();
+    }
+
+    public function getStatusMessage()
+    {
+        return '';
+    }
+
+    public function canCancel()
+    {
+        return false;
+    }
+
+    public function canTriggerByChoice()
+    {
+        return false;
+    }
+}
--- upload/src/addons/SV/AlertImprovements/XF/Repository/UserAlertPatch.php
+++ upload/src/addons/SV/AlertImprovements/XF/Repository/UserAlertPatch.php
@@ -14,43 +14,38 @@ class UserAlertPatch extends XFCP_UserAlertPatch
 {
     protected $svBatchLimit = 50000;
 
-    public function pruneReadAlerts($cutOff = null)
+    public function pruneViewedAlertsBatch(int $cutOff, float $startTime, float $maxRunTime): bool
     {
-        if ($cutOff === null)
+        if (!$cutOff)
         {
-            $cutOff = \XF::$time - $this->options()->alertExpiryDays * 86400;
+            return false;
         }
-        $maxRunTime = max(min(\XF::app()->config('jobMaxRunTime'), 4), 1);
-        $startTime = \microtime(true);
+        $db = $this->db();
         do
         {
-            // TODO; rebuild view/unread counts
             /** @var AbstractStatement $statement */
-            $statement = $this->db()->executeTransaction(function (AbstractAdapter $db) use ($cutOff) {
+            $statement = $db->executeTransaction(function (AbstractAdapter $db) use ($cutOff) {
                 return $db->query("DELETE FROM xf_user_alert WHERE view_date > 0 AND view_date < ? LIMIT {$this->svBatchLimit}", $cutOff);
             }, AbstractAdapter::ALLOW_DEADLOCK_RERUN);
 
             if (microtime(true) - $startTime >= $maxRunTime)
             {
-                \XF::app()->jobManager()->enqueueLater(null, \XF::$time + 1, 'SV\AlertImprovements:AlertCleanup', [], false);
-
-                return;
+                return true;
             }
         }
         while ($statement && $statement->rowsAffected() >= $this->svBatchLimit);
+
+        return false;
     }
 
-    public function pruneUnreadAlerts($cutOff = null)
+    public function pruneUnviewedAlertsBatch(int $cutOff, float $startTime, float $maxRunTime): bool
     {
-        if ($cutOff === null)
+        if (!$cutOff)
         {
-            $cutOff = \XF::$time - 30 * 86400;
+            return false;
         }
-        $maxRunTime = max(min(\XF::app()->config('jobMaxRunTime'), 4), 1);
-        $startTime = \microtime(true);
         do
         {
-            // TODO; rebuild view/unread counts
             /** @var AbstractStatement $statement */
             $statement = $this->db()->executeTransaction(function (AbstractAdapter $db) use ($cutOff) {
                 return $db->query("DELETE FROM xf_user_alert WHERE view_date = 0 AND event_date < ? LIMIT {$this->svBatchLimit}", $cutOff);
@@ -58,11 +53,56 @@ class UserAlertPatch extends XFCP_UserAlertPatch
 
             if (microtime(true) - $startTime >= $maxRunTime)
             {
-                \XF::app()->jobManager()->enqueueLater(null, \XF::$time + 1, 'SV\AlertImprovements:AlertCleanup', [], false);
-
-                return;
+                return true;
             }
         }
         while ($statement && $statement->rowsAffected() >= $this->svBatchLimit);
+
+        return false;
     }
+
+    public function pruneViewedAlerts($cutOff = null)
+    {
+        if ($cutOff === null)
+        {
+            $cutOff = \XF::$time - $this->options()->alertExpiryDays * 86400;
+        }
+        \XF::app()->jobManager()->enqueueLater('sViewedAlertCleanup', \XF::$time + 1, 'SV\AlertImprovements:ViewedAlertCleanup', [
+            'cutOff' => $cutOff,
+        ], false);
+    }
+
+    public function pruneUnviewedAlerts($cutOff = null)
+    {
+        if ($cutOff === null)
+        {
+            $unviewedAlertExpiryDays = isset(\XF::options()->svUnviewedAlertExpiryDays) ? \XF::options()->svUnviewedAlertExpiryDays : 30;
+            $cutOff = \XF::$time - $unviewedAlertExpiryDays * 86400;
+        }
+        \XF::app()->jobManager()->enqueueLater('svUnviewedAlertCleanup', \XF::$time + 1, 'SV\AlertImprovements:UnviewedAlertCleanup', [
+            'cutOff' => $cutOff,
+        ], false);
+    }
+
+
+    /**
+     * XF2.1 shim
+     *
+     * @param int|null $cutOff
+     */
+    public function pruneReadAlerts($cutOff = null)
+    {
+        $this->pruneViewedAlerts($cutOff);
+    }
+
+    /**
+     * XF2.1 shim
+     *
+     * @param int|null $cutOff
+     */
+    public function pruneUnreadAlerts($cutOff = null)
+    {
+        $this->pruneUnviewedAlerts($cutOff);
+    }
+
 }
