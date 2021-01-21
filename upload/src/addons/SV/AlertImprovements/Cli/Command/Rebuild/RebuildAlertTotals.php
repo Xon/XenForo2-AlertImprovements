86433f6643c88ade3ebf5245c402c02027121930
Xon <tazipper1@hotmail.com>
2021-01-21 12:18:47 +0800

    Add CLI command "xf-rebuild:sv-alert-totals" to expose rebuilding alert totals job

---
+++ upload/src/addons/SV/AlertImprovements/Cli/Command/Rebuild/RebuildAlertTotals.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace SV\AlertImprovements\Cli\Command\Rebuild;
+
+use Symfony\Component\Console\Input\InputOption;
+use XF\Cli\Command\Rebuild\AbstractRebuildCommand;
+
+class RebuildAlertTotals extends AbstractRebuildCommand
+{
+
+    protected function getRebuildName() : string
+    {
+        return 'sv-alert-totals';
+    }
+
+    protected function getRebuildDescription() : string
+    {
+        return 'Rebuilds alert totals';
+    }
+
+    protected function getRebuildClass() : string
+    {
+        return 'SV\AlertImprovements:AlertTotalRebuild';
+    }
+
+    protected function configureOptions()
+    {
+        $this->addOption(
+            'pendingRebuilds',
+            null,
+            InputOption::VALUE_REQUIRED,
+            'Rebuild all users or just users with pending rebuilds after alert pruning',
+            true
+        );
+    }
+}
